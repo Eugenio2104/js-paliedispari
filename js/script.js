b37b95a3a01088ffb3d0f8c79a75034544dd6a1d
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-19 16:43:10 +0200

    prova

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function cercaPalindromo(stringa){
+  let j = stringa.length -1;
+  for(let i = 0; i < j/2; i++){
+    let inizio = stringa[i]; //caratteri iniziali
+    let fine = stringa[j-i];//caratteri finali
+    if(inizio != fine){
+      return false;
+    }
+  }
+  return true;
+}
+
+function isPalindromo(stringa){
+  let element = cercaPalindromo(stringa);
+  if(element == true){
+    
+    console.log(`la stringa è palindroma`);
+  }else{
+    console.log(`la stringa non è palindorma`);
+  }
+}
+
+let test = "annaaa";
+isPalindromo(test);
